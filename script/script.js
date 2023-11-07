3b45a6e013f31af5cf348645a280ea5bed14ca62
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-11-08 00:19:56 +0100

    last commit

--- script/script.js
+++ script/script.js
@@ -1,55 +1,77 @@
 // ESERCIZIO 1
 
-window.addEventListener("DOMContentLoaded", () => {
-  const saveButton = document.getElementById("saveButton");
-  const resetButton = document.getElementById("resetButton");
-  const btnResetInput = document.getElementsByClassName("resetBtn");
-  const txtInput = document.querySelector("input[type='text']");
+// window.addEventListener("DOMContentLoaded", () => {
+//   const saveButton = document.getElementById("saveButton");
+//   const resetButton = document.getElementById("resetButton");
+//   const btnResetInput = document.getElementsByClassName("resetBtn");
+//   const txtInput = document.querySelector("input[type='text']");
 
-  saveButton.onclick = setSaveButton;
-  resetButton.onclick = remResetButton;
-  txtInput.onchange = handleInput;
-  btnResetInput.onclick = () => {
-    localStorage.removeItem("searchQuery");
-    txtInput.value = "";
-  };
-  loadData();
-  preLoadText(txtInput);
-});
+//   saveButton.onclick = setSaveButton;
+//   resetButton.onclick = remResetButton;
+//   txtInput.onchange = handleInput;
+//   btnResetInput.onclick = () => {
+//     localStorage.removeItem("searchQuery");
+//     txtInput.value = "";
+//   };
+//   loadData();
+//   preLoadText(txtInput);
+// });
 
-const preLoadText = (inputNode) => {
-  const storedText = localStorage.getItem("searchQuery");
-  if (storedText && inputNode) {
-    inputNode.value = storedText;
-  }
-};
+// const preLoadText = (inputNode) => {
+//   const storedText = localStorage.getItem("searchQuery");
+//   if (storedText && inputNode) {
+//     inputNode.value = storedText;
+//   }
+// };
 
-const loadData = () => {
-  const dataInStorage = localStorage.getItem("save");
-  const html = document.documentElement;
+// const loadData = () => {
+//   const dataInStorage = localStorage.getItem("save");
+//   const html = document.documentElement;
 
-  if (dataInStorage) {
-    html.classList.add("data");
+//   if (dataInStorage) {
+//     html.classList.add("data");
+//   } else {
+//     html.classList.remove("data");
+//   }
+// };
+
+const check = function () {
+  if (localStorage.getItem("usr")) {
+    txt = "Ciao " + localStorage.getItem("usr") + "!";
   } else {
-    html.classList.remove("data");
+    txt = "No data";
   }
+  document.querySelector(".div1").innerHTML = txt;
 };
 
-const setSaveButton = () => {
-  localStorage.setItem("save", "data");
-
-  loadData();
+const save = function () {
+  let usr = document.getElementById("usr").value;
+  localStorage.setItem("usr", usr);
+  check();
 };
 
-const remResetButton = () => {
-  localStorage.removeItem("save");
-
-  loadData();
+const deleteData = function () {
+  localStorage.removeItem("usr");
+  check();
 };
 
-const handleInput = (event) => {
-  localStorage.setItem("searchQuery", event.target.value);
-};
+window.onload = check;
+
+// const setSaveButton = () => {
+//   localStorage.setItem("save", "data");
+
+//   loadData();
+// };
+
+// const remResetButton = () => {
+//   localStorage.removeItem("save");
+
+//   loadData();
+// };
+
+// const handleInput = (event) => {
+//   localStorage.setItem("searchQuery", event.target.value);
+// };
 
 // ESERCIZIO 2
 
